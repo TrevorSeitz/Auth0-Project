dade5744ab86af78aa9d53ce905ef8bc52848ba6
Trevor Seitz <trevinla@gmail.com>
2022-07-22 18:05:02 -0400

    getting form printed

--- 01-Login/CombineLists.py
+++ 01-Login/CombineLists.py
@@ -11,16 +11,31 @@ def main_list():
     length = len(client_list)
     for i in range(length):
         if client_list[i] == "All Applications":
-            print("main_list - ", main_list)
+            # print("main_list - ", type(main_list))
+            # iterate(main_list)
             return main_list
         elif (i % 2) == 0:
-            client = [client_list[i]]
+            client = {}
+            client.update({"Name":client_list[i]})
             main_list.append(client)
+            # print("Client - ", type(client))
         elif (i % 2) != 0:
             actlength = len(actionlist)
             for j in range(actlength):           
                 if client_list[i] in actionlist[j]["code"]:
-                    client.append(actionlist[j]["Action Name"])
+                    # print("Action - ", type(actionlist[j]))
+                    client.update({"Action Name":actionlist[j]["Action Name"]})
+                    # print(client)
+                else: client.update({"Action Name":"Nothing Specified"})
+
+# def iterate(main_list):
+#     for i in range(len(main_list)):
+#         for key, value in main_list[i].items():
+#             # print(key, value)
+#             if key == "Name":
+#                 # print("\n", "\n", value)
+#             elif key == "Action Name":
+#                 # print("  - ", value)
 
 main_list()
 
