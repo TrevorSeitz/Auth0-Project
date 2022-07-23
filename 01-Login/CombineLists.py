fb7dcac4dcc6a9b21baf4af05470a99a6a9a8acf
Trevor Seitz <trevinla@gmail.com>
2022-07-23 19:16:40 -0400

    fixed json conversion - made actions a dictionary

--- 01-Login/CombineLists.py
+++ 01-Login/CombineLists.py
@@ -1,41 +1,34 @@
 from asyncio import events
 from GetDetails import get_client_names, get_action_list
 
-client_list = get_client_names()
-action_list = get_action_list()
+# client_list = get_client_names()
+# action_list = get_action_list()
 
 def main_list():    
     client_list = get_client_names()
-    actionlist = get_action_list()
+    action_list = get_action_list()
     main_list = []
     length = len(client_list)
     for i in range(length):
+        client = {}
+        actions = []
         if client_list[i] == "All Applications":
-            # print("main_list - ", type(main_list))
+            print("main_list - ", main_list)
             # iterate(main_list)
             return main_list
         elif (i % 2) == 0:
-            client = {}
             client.update({"Name":client_list[i]})
             main_list.append(client)
-            # print("Client - ", type(client))
-        elif (i % 2) != 0:
-            actlength = len(actionlist)
-            for j in range(actlength):           
-                if client_list[i] in actionlist[j]["code"]:
-                    # print("Action - ", type(actionlist[j]))
-                    client.update({"Action Name":actionlist[j]["Action Name"]})
-                    # print(client)
-                else: client.update({"Action Name":"Nothing Specified"})
+        alength = len(action_list)
+        # print("i - ", i)
+        for j in range(alength):    
+            # print("j - ", j)       
+            if client_list[i] in action_list[j]["code"]:
+                # print("Action - ", action_list[j])
+                actions.append({"Action Name":action_list[j]["Action Name"]})
+            client.update({"Actions":actions})
+            # print(i, " - client - ", client)
 
-# def iterate(main_list):
-#     for i in range(len(main_list)):
-#         for key, value in main_list[i].items():
-#             # print(key, value)
-#             if key == "Name":
-#                 # print("\n", "\n", value)
-#             elif key == "Action Name":
-#                 # print("  - ", value)
-
+    
 main_list()
 
--- 01-Login/GetActions.py
+++ 01-Login/GetActions.py
@@ -8,11 +8,6 @@ from authlib.integrations.flask_client import OAuth
 from dotenv import find_dotenv, load_dotenv
 from flask import Flask, redirect, render_template, session, url_for
 
-# client_id=env.get("AUTH0_CLIENT_ID"),
-# client_secret=env.get("AUTH0_CLIENT_SECRET"),
-# path = env.get("AUTH0_DOMAIN")
-# manage_secret = env.get("AUTH0_MANAGE_SECRET")
-
 ENV_FILE = find_dotenv()
 if ENV_FILE:
     load_dotenv(ENV_FILE)
@@ -33,20 +28,15 @@ def action_list():
             }
     headers = { 'content-type': "application/json","authorization": token }
 
-
     conn.request("GET", "/api/v2/actions/actions", urlencode(payload), headers)
     res = conn.getresponse()
     data = res.read()
-    result_data_json = data.decode('utf8').replace("'", '"')
-
-    # print("result_data_json - ", result_data_json, "GETACTIONS - ", type(result_data_json))
+    result_data_json = data.decode('utf8').replace("/'/g", '"') # replace single quotes with double quotes
     # Load the JSON to a Python list & dump it back out as formatted JSON
     result_data = json.loads(result_data_json)
     action_list = result_data
     
-    
-    # print("GetActions  -  action_list - ", action_list)
     return action_list
 
 # action_list(AUTH0_CLIENT_ID, AUTH0_CLIENT_SECRET, AUTH0_API_IDENTIFIER, AUTH0_DOMAIN)
-action_list()
+# action_list()
