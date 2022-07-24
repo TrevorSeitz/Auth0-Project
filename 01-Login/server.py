7b3c438791b0abadc5338b6c3c752afdba81f2db
Trevor Seitz <trevinla@gmail.com>
2022-07-24 13:25:43 -0400

    iterating action names works on home-*

--- 01-Login/server.py
+++ 01-Login/server.py
@@ -78,7 +78,7 @@ def home():
         "home.html",
         session=session.get("user"),
         pretty=json.dumps(session.get("user"), indent=4),
-        client_list =json.dumps(client_action_list, indent=4),
+        # client_list =json.dumps(client_action_list, indent=4),
         client_list2 =client_action_list,
         
     )
